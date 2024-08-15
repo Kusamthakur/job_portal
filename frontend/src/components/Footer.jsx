59c8e222b7a192c522d58ebc7c694f2234ca2301
Kusamthakur <kusamthakur433@gmail.com>
2024-08-16 02:16:03 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
+
+import logo from "../images/JB_logo.png"
+
+const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <footer>
+        <div>
+        <img  id="logof" src={logo} alt="logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>new guru nanak nagar jalandhar , punjab</li>
+            <li>Kusamthakur433@gmail.com</li>
+            <li>6239260239</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>Youtube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By CodeWith Kusamthakur
+      </div>
+    </>
+  );
+};
+
+export default Footer;
